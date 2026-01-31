47f5b7f15ed184478729ee3bb97f43f79aceb390
chanseon.park <chanseon.park@kongstudios.net>
2026-01-31 13:24:35 +0900

    성능 테스트 버튼 추가

---
+++ Assets/Scripts/PerformanceTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PerformanceTest : MonoBehaviour
+{
+    public GameObject testPrefab;
+    public Vector3 offset = new Vector3(1, 0, 0);
+ 
+    public Vector3 startPos;
+    public void SpawnTestPrefab()
+    {
+        var newPrefab = Instantiate(testPrefab);
+
+        newPrefab.transform.SetParent(Camera.main.transform);
+        newPrefab.transform.localPosition = startPos;
+        startPos += offset;
+        newPrefab.transform.Rotate(Vector3.up, Random.Range(0, 360));
+        
+    }
+}
