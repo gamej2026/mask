9bf83b814fbba17fcbcdb630e79fb649d17379e7
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2026-02-01 10:32:52 +0900

    게임 클리어, 게임 오버 시 일러스트 암전되었다가 보여주는 형식으로 수정

--- Assets/Scripts/UI/GameOverClearPanel.cs
+++ Assets/Scripts/UI/GameOverClearPanel.cs
@@ -1,7 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameOverClearPanel : MonoBehaviour
 {
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+
+    void OnEnable()
+    {
+        if (fadeImage != null)
+        {
+            // 알파값 1로 시작
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, 1f);
+            // 점차 0으로 페이드 아웃
+            fadeImage.DOFade(0f, fadeDuration).SetEase(Ease.OutQuad);
+        }
+    }
+
     public void OnClickMainMenu()
     {
         GameManager.Instance.GoToMain();
